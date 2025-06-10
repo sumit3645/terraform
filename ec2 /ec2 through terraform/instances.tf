4326bf12021248b69fed65e238da0b3ca0310f2b
sumit3645 <154312428+sumit3645@users.noreply.github.com>
2025-06-10 19:07:48 +0530

    Update instances.tf

--- ec2 /ec2 through terraform/instances.tf
+++ ec2 /ec2 through terraform/instances.tf
@@ -19,6 +19,13 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks=["0.0.0.0/0"]
     }
   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 
