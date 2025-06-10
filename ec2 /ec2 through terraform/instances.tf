fb23c0719c484b190e68d6f41c2474abfa316a1d
sumit3645 <154312428+sumit3645@users.noreply.github.com>
2025-06-10 17:43:37 +0530

    Create instances.tf
    attach aws-key and security_group to instanes

---
+++ ec2 /ec2 through terraform/instances.tf
@@ -0,0 +1,35 @@
+#creating ssh-key for instances
+resource "aws_key_pair" "key_tf" {
+  key_name   = "sumit-key"
+  public_key = file("${path.module}/id_rsa")
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  dynamic "ingress"{
+    for_each=[22,80,443,3306]
+    iterator=port
+    content {
+    description="TLS from vpc"
+    from_port=port.value
+    to_port=port.value
+    protocol="tcp"
+    cidr_block=["0.0.0.0/0"]
+    }
+  }
+}
+
+
+
+
+resource "aws_instance" "app-server" {
+  ami           = "ami-02521d90e7410d9f0"
+  instance_type = "t2.micro"
+  key_name="${aws_key_pair.key_tf.key_name}"
+  vpc_security_group=["${aws_security_group.allow_tls.id}"]
+  tags = {
+    Name = "terraform-demo"
+  }
+}
